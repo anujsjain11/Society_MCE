1265a7ddbd69f3c1d4ab0d3654db1931cdca915e
AnujSJain <anujsjain11@gmail.com>
2026-03-11 03:52:02 +0530

    add: code to convert for ocr json to label studio json

--- Prototype/main.py
+++ Prototype/main.py
@@ -1,3 +1,5 @@
+import json
+
 from paddleocr import PaddleOCR
 
 import os
@@ -10,19 +12,92 @@ ocr = PaddleOCR(
     use_textline_orientation=False) # text detection + text recognition
 
 
-# ocr = PaddleOCR(use_doc_orientation_classify=True, use_doc_unwarping=True) # text image preprocessing + text detection + textline orientation classification + text recognition
-# ocr = PaddleOCR(use_doc_orientation_classify=False, use_doc_unwarping=False) # text detection + textline orientation classification + text recognition
-# ocr = PaddleOCR(
-#     text_detection_model_name="PP-OCRv5_mobile_det",
-#     text_recognition_model_name="PP-OCRv5_mobile_rec",
-#     use_doc_orientation_classify=False,
-#     use_doc_unwarping=False,
-#     use_textline_orientation=False) # Switch to PP-OCRv5_mobile models
+
+image_path = "./training data/1.jpg"
 
 
-result = ocr.predict("./training data/1.jpg")
+result = ocr.predict(image_path) # predict the image
 for res in result:
     res.print() # print the result of each image
-    print("-----")
-    res.save_to_img("output")
-    res.save_to_json("output")
+    # res.save_to_img("output")
+    res.save_to_json("output/paddle_ocr_result.json")
+
+
+
+ocr_data = []
+
+for line in result[0]:
+    box, (text, score) = line
+
+    xs = [p[0] for p in box]
+    ys = [p[1] for p in box]
+
+    x_min = min(xs)
+    y_min = min(ys)
+    x_max = max(xs)
+    y_max = max(ys)
+
+    ocr_data.append({
+        "text": text,
+        "bbox": [x_min, y_min, x_max, y_max]
+    })
+
+task = [{
+    "data": {
+        "image": image_path,
+        "ocr": ocr_data
+    }
+}]
+
+with open("labelstudio_tasks.json", "w") as f:
+    json.dump(task, f, indent=4)
+
+
+
+
+
+# with open("output/paddle_ocr_result.json", "r") as f:
+#     data = json.load(f)
+#     # detect correct polygon key
+#     # polys = data.get("input_path") 
+#     # print(polys)
+
+
+# texts = data["rec_texts"]
+# scores = data["rec_scores"]
+# boxes = data["dt_polys"]
+
+# filtered = []
+
+# for text, score, box in zip(texts, scores, boxes):
+#     if score > 0.8:
+#         new_data = {
+#     "data": {
+#         "ocr": {
+#             "texts": data["rec_texts"],
+#             "boxes": data["dt_polys"],
+#             "scores": data["rec_scores"]
+#         }
+#     }
+# }
+
+#         # filtered.append({
+#         #     "text": text,
+#         #     "score": score,
+#         #     "box": box
+#         # })
+
+
+
+
+# with open("output/fixed_ocr.json", "w") as f:
+#     json.dump(new_data, f, indent=2)
+
+
+
+
+
+# for item in filtered:
+#     print(item)
+
+
