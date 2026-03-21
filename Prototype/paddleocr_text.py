354ab41646734e4f88aa5d737b22647b5fe83a15
AnujSJain <anujsjain11@gmail.com>
2026-03-21 10:39:42 +0530

    trying to implement the layoutlvm3

---
+++ Prototype/paddleocr_text.py
@@ -0,0 +1,20 @@
+import json
+from paddleocr import PaddleOCR
+import os
+image_path = "./training data/1.jpg"
+def extract_ocr_data(image_path):
+    # Disable model source check to allow loading models from custom paths
+    os.environ["PADDLE_PDX_DISABLE_MODEL_SOURCE_CHECK"] = "True"
+
+
+    ocr = PaddleOCR(
+        use_doc_orientation_classify=False, 
+        use_doc_unwarping=False, 
+        use_textline_orientation=False) # text detection + text recognition
+
+
+    result = ocr.predict(image_path) # predict the image
+    for res in result:
+        res.print() # print the result of each image
+        # res.save_to_img("output")
+        res.save_to_json("output/paddle_ocr_result.json")
