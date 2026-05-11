9f08e78f118ed00eeb7a76f46c408c32347ac864
AnujSJain <anujsjain11@gmail.com>
2026-05-12 04:42:19 +0530

    spliting large file to small class and file

---
+++ Prototype/.ipynb_checkpoints/LLMParser-checkpoint.py
@@ -0,0 +1,65 @@
+from llama_cpp import Llama
+
+
+class LLMParser:
+
+    def __init__(self):
+
+        self.llm = Llama(
+            model_path="llama3-Q5_K_M.gguf",
+            n_ctx=2048,
+            n_threads=8,
+            verbose=False
+        )
+
+    def extract_receipt(self, ocr_result):
+
+        ocr_text = "\n".join(ocr_result)
+
+        prompt = f"""
+You are a receipt extraction API.
+
+Extract receipt items from OCR text.
+
+Return ONLY valid JSON.
+No markdown.
+No explanation.
+No extra text.
+No notes.
+No python code.
+
+JSON schema:
+{{
+  "items": [
+    {{
+      "product": "string",
+      "quantity": "string",
+      "price": 0
+    }}
+  ],
+  "total_quantity": 0,
+  "total_price": 0
+}}
+
+Rules:
+- Extract ALL products
+- Merge multiline products
+- Ignore noise
+- Extract quantity separately
+- Extract total separately
+- Price must be numeric
+- Do NOT hallucinate products
+
+OCR:
+{ocr_text}
+"""
+
+        output = self.llm(
+            prompt,
+            max_tokens=500,
+            temperature=0.1
+        )
+
+        result = output["choices"][0]["text"]
+
+        return result
--- Prototype/.ipynb_checkpoints/OCREngine-checkpoint.py
+++ Prototype/.ipynb_checkpoints/OCREngine-checkpoint.py
@@ -32,3 +32,75 @@ class OCREngine:
         result = self.ocr.predict(img)
 
         return result
+        
+    def downscaling(self, poly_list):
+
+        poly_downscaling = []
+
+        for poly in poly_list:
+
+            # convert to float
+            poly = np.array(poly, dtype=np.float32)
+
+            # draw original polygon
+            cv2.polylines(
+                output,
+                [np.array(poly, dtype=np.int32)],
+                isClosed=True,
+                color=(255, 0, 0),
+                thickness=2
+            )
+
+            # bounding box
+            x_min, y_min = np.min(poly, axis=0)
+            x_max, y_max = np.max(poly, axis=0)
+
+            w = x_max - x_min
+            h = y_max - y_min
+
+            # adaptive shrink
+            shrink_factor_h = max(0.6, min(0.9, 1 - 20 / w))
+            shrink_factor_v = max(0.6, min(0.9, 1 - 20 / h))
+
+            # center
+            center_x = np.mean(poly[:, 0])
+            center_y = np.mean(poly[:, 1])
+
+            poly_box = []
+
+            for (x, y) in poly:
+
+                x_new = int(
+                    center_x + (x - center_x) * shrink_factor_h
+                )
+
+                y_new = int(
+                    center_y + (y - center_y) * shrink_factor_v
+                )
+
+                poly_box.append([x_new, y_new])
+
+            poly_downscaling.append(
+                np.array(poly_box, dtype=np.int16)
+            )
+
+        return poly_downscaling
+
+    def downscaling_poly_ocr_result(self, result):
+
+        # downscale polygons
+        downscaling_poly = self.downscaling(
+            result[0]['dt_polys']
+        )
+        
+        # combine polygon + text + score
+        ocr_result = [
+            (poly, text, scores)
+            for poly, text, scores in zip(
+                downscaling_poly,
+                result[0]['rec_texts'],
+                result[0]['rec_scores']
+            )
+        ]
+        
+        return ocr_result
